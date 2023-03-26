f77b85bccec90317156e42141f814f15b4b34288
Neonn7528 <Neonn7@yandex.ru>
2023-03-26 12:52:35 +0300

    выводит случ 3начное число и удаляет 2. цифру этого числа

---
+++ Lesson2.1/Program.cs
@@ -0,0 +1,12 @@
+﻿/*Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+● 456 -> 46
+● 782 -> 72
+● 918 -> 98*/
+Random nomber = new Random();
+int nomberValue = nomber.Next(100, 1000);
+int last = nomberValue / 100;
+int result = last * 10;
+int firstDigit = nomberValue % 10;
+int result1 = result + firstDigit; 
+Console.WriteLine(nomberValue);
+Console.WriteLine(result1);
