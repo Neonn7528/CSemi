7264eccc7af45613dc2e7947c0590824c9267483
Neonn7528 <Neonn7@yandex.ru>
2023-03-26 10:03:10 +0300

    задач 1 домашки

---
+++ HomeWorke/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Напишите прогу, которая на выход принемает
+два числа и выдает какое больше, а какое меньше.*/
+int max = 0;
+int min = 0;
+Console.WriteLine("Введите первое число");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = int.Parse(Console.ReadLine());
+if( a > b ) { 
+    max = a; min = b;
+}
+else { max = b; min = a;
+ } 
+Console.Write("max = ");
+Console.WriteLine(max);
+Console.Write("min = ");
+Console.WriteLine(min);
