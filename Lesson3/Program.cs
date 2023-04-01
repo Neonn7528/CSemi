b177b2f3d757048f8df466bb52e022cbb86c27e7
Neonn7528 <Neonn7@yandex.ru>
2023-04-01 12:23:55 +0300

    Х и У в 2Д пространстве

---
+++ Lesson3/Program.cs
@@ -0,0 +1,129 @@
+﻿/*Console.WriteLine("Привет!!!");
+Console.WriteLine("Введите х");
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите у");
+int y = int.Parse(Console.ReadLine());
+int result = GetNumberOfQarter(x, y);
+if (result == 0)
+if (x == 0 || y == 0)
+    {
+       Console.WriteLine("х или у равны нулю."); 
+       return;
+    }
+    int GetNumberOfQarter{int x, int y}
+    {{
+        if (x>0 && y > 0)
+        return 1;
+    }
+        {
+        if (x<0 && y > 0)
+        return 2;
+    }
+    {
+        if (x>0 && y < 0)
+        return 3;
+    }
+    {
+        if (x<0 && y < 0)
+        return 4;
+    }
+    return 0;}*/
+
+/*Задача No18. Общее обсуждение
+Напишите программу, которая по заданному номеру четверти,
+ показывает диапазон возможных координат точек в этой четверти (x и y).*/
+
+/*Console.WriteLine("Введите номер четверти:");
+int quarter = int.Parse(Console.ReadLine());
+
+string result = GetXY(quarter);
+
+if (result == "0")
+{
+    Console.Write("Номер четверти введен не правильно");
+    return;
+}
+
+Console.Write("В четверть попадаюат: ");
+Console.Write(result);
+
+
+string GetXY(int quarter)
+{
+    if (quarter == 1)
+{
+    return "X > 0 и Y > 0";
+}
+
+if (quarter == 2)
+{
+    return "X < 0 и Y > 0";   
+}
+
+if (quarter == 3)
+{
+    return "X < 0 и Y < 0";    
+}
+
+if (quarter == 4)
+{
+    return "X > 0 и Y < 0";    
+}    
+
+return "0";
+}/*
+
+
+/*Задача No21. Работа в группах
+Напишите программу, которая принимает на вход координаты двух точек
+ и находит расстояние между ними в 2D пространстве.
+● A (3,6); B (2,1) -> 5,09
+● A (7,-5); B (1,-1) -> 7,21*/
+
+/*Console.WriteLine("Введите X1:");
+int X1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите Y1:");
+int Y1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите X2:");
+int X2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите Y2:");
+int Y2 = int.Parse(Console.ReadLine());
+
+double result = GetDistance(X1, X2, Y1, Y2);
+
+if (result == 0)
+{
+    Console.Write("Переменные введены не правильно");
+    return;
+}
+
+Console.Write("Расстояние между точками: ");
+Console.Write(result);
+
+
+
+double GetDistance(int X1, int X2, int Y1, int Y2)
+{
+
+if (X1 == 0 & Y1 == 0 & X2 == 0 & Y2 == 0)
+{
+    return 0;
+}
+
+double L;
+
+L = Math.Sqrt(Math.Pow(X2 - X1, 2) + Math.Pow(Y2 - Y1, 2));
+
+return L;
+
+}*/
+
+/*Напишите программу, которая принимает на вход число
+ (N) и выдаёт таблицу квадратов чисел
+от 1 до N.
+● 5 -> 1, 4, 9, 16, 25.
+● 2 -> 1,4*/
+
