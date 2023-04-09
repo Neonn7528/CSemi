552b82b5d4b8041acbeb7a6dae1277692ed055b3
Neonn7528 <Neonn7@yandex.ru>
2023-04-09 06:19:29 +0300

    Задайте массиы из 12 элементов и найдите сумму полож и отриц

---
+++ Lesson5/Program.cs
@@ -0,0 +1,55 @@
+﻿/*Задача31. Задайте массиы из 12 
+элементов и найдите сумму положительных
+и сумму отрицательных.*/
+int[] array = GenerateArray(12);
+PrintArray(array);
+
+int sum = CalculatePositiveSum(array);
+int sumNegativeNumbers = CalculateNegativeSum(array);
+Console.WriteLine($"Сумма положительных равна {sum}");
+Console.WriteLine($"Сумма отрицательных равна{sumNegativeNumbers}");
+
+
+int[] GenerateArray(int length)//генерация массива
+{
+    int[] array = new int[length];
+
+    Random random = new Random();
+
+    for (var i = 0; i < length; i++)
+    {
+        array[i] = random.Next(-9, 10);
+    }
+
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.WriteLine($"[{string.Join(", ", array)}]");
+}
+
+
+int CalculateNegativeSum(int[] array)
+{
+    int sumNegative = 0;
+    for (var i = 0; i < array.Length; i++)
+    {
+        if(array[i] < 0){
+            sumNegative+=array[i];
+        }
+    }
+    return sumNegative;
+}
+    int CalculatePositiveSum(int[] array)
+{
+    int sumPositive = 0;
+    for (var i = 0; i < array.Length; i++)
+    {
+        if(array[i] > 0){
+            sumPositive+=array[i];
+        }
+    }
+
+    return sumPositive;
+}
